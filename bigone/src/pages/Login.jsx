0ef3c33d0db31bc9a3b3ed810b58442e6d2564fc
kniiiiko <dywjd1128@naver.com>
2025-08-05 15:47:24 +0900

    로그인 및 회원가입 구현

---
+++ bigone/src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as L from "../styles/StyledLogin";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const goSignUp = () => {
+    navigate(`/signup`);
+  };
+
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const isActive = id.length > 0 && pw.length > 0;
+
+  return (
+    <L.Container>
+      <L.Logo>순삭</L.Logo>
+
+      <L.Input>
+        <img src={`${process.env.PUBLIC_URL}/images/Id.svg`} />
+        <input placeholder="아이디" value={id} onChange={(e) => setId(e.target.value)}></input>
+      </L.Input>
+      <L.Input>
+        <img src={`${process.env.PUBLIC_URL}/images/Password.svg`} />
+        <input placeholder="비밀번호(8자 이상)" type="password" value={pw} onChange={(e) => setPw(e.target.value)}></input>
+        <img src={`${process.env.PUBLIC_URL}/images/Eye-off.svg`} />
+      </L.Input>
+      <L.LoginBtn style={{ marginTop: "60px", background: isActive ? "#FF4F26" : "#C4C4C4", cursor: isActive ? "pointer" : "default" }}>로그인</L.LoginBtn>
+      <L.SignUpBtn onClick={goSignUp}>회원가입</L.SignUpBtn>
+    </L.Container>
+  );
+};
+
+export default Login;
---
+++ bigone/src/styles/StyledSignUp.jsx
@@ -0,0 +1,134 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  margin: 0 auto;
+  margin-top: 0px;
+  min-height: 100vh;
+  padding: 0; /* 불필요한 패딩 제거 */
+  box-sizing: border-box; /* 패딩이 width에 포함되도록 설정 */
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  width: 393px;
+  flex-shrink: 0;
+  padding-bottom: 200px;
+`;
+
+export const Header = styled.div`
+  height: 80px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding: 17px; /* 아이콘 여백 조절으로 인해 변경 */
+`;
+
+export const Title = styled.div`
+  margin-top: 6px;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.8px;
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  margin: 0 15px;
+  gap: 20px;
+  align-items: center;
+
+  #back {
+    width: 10px;
+    height: 21px;
+  }
+`;
+
+export const InputCon = styled.div`
+  padding: 10px 30px;
+  flex-direction: row;
+
+  #title {
+    color: #000;
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+
+  input {
+    margin-top: 12px;
+    margin-bottom: 10px;
+    padding: 15px;
+    width: 100%;
+    height: 50px;
+    flex-shrink: 0;
+    border-radius: 5px;
+    background: rgba(196, 196, 196, 0.3);
+    color: #969696;
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    border: 1px solid rgba(196, 196, 196, 0.3);
+  }
+
+  input:focus {
+    outline: none;
+    box-shadow: none;
+    border-color: #ff4f26;
+  }
+
+  #inputDetail {
+    color: #969696;
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+  }
+
+  #IdCheckBtn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 35%;
+    height: 50px;
+    flex-shrink: 0;
+    border-radius: 5px;
+    background: #c4c4c4;
+    color: #fff;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+`;
+
+export const IdChechBtn = styled.div``;
+
+export const SignUpBtn = styled.div`
+  position: fixed;
+  display: flex; /* 내부 요소 정렬 자유롭게 */
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+
+  width: 393px;
+  height: 100px;
+  flex-shrink: 0;
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.8px;
+`;
